11666b264d955797d5ab68c48da9f38b032b9a49
Daun2121 <rayruby210203@gmail.com>
2025-04-09 14:40:51 +0700

    hum menu aja

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,18 +18,41 @@ const Navbar = () => {
       <nav className="mx-auto lg:flex justify-between items-center 2xl:max-w-[1536px] font-monument">
         <div className="flex items-center justify-between">
           {/* Logo */}
-          <a href='#Home' className="text-[32px]"><p>PT. SARANA GEMA REKAYASA</p></a>
+          <a href='#Home' 
+            className="
+            
+            text-[28px]
+            lg:text-[24px]
+            whitespace-nowrap 
+            xl:text-[32px] 
+            ">
+            PT. SARANA GEMA REKAYASA
+          </a>
 
           {/* Menu toggle button */}
           <input className="hidden peer" type="checkbox"/>
-          <label aria-expanded={menuOpen} onClick={handleMenuToggle} 
-          className='w-10 h-9 cursor-pointer flex flex-col items-center justify-center lg:hidden'>
-            <div className={`w-[65%] h-[3px] translate-y-[0.65rem] bg-white rounded-sm transition-all duration-300 origin-left lg:hidden
-            ${menuOpen ? 'rotate-[-45deg]' : ''}`}></div>
-            <div className={`w-[65%] h-[3px] bg-white rounded-md transition-all duration-300 origin-center lg:hidden
-            ${menuOpen ? 'hidden' : ''}`}></div>
-            <div className={`w-[65%] h-[3px] -translate-y-[0.65rem] bg-white rounded-md transition-all duration-300 origin-left lg:hidden
-            ${menuOpen ? 'rotate-[45deg]' : ''}`}></div>
+          <label 
+            aria-expanded={menuOpen} 
+            onClick={handleMenuToggle} 
+            className='w-10 h-10 cursor-pointer flex flex-col items-center justify-center lg:hidden'>
+              <div className={`
+                w-[65%] h-[3px] 
+                bg-white rounded-sm 
+                translate-y-[0.65rem] 
+                transition-all duration-300 origin-left 
+                lg:hidden
+              ${menuOpen ? 'rotate-[-45deg]' : ''}`}>
+              </div>
+              <div className={`
+                w-[65%] h-[3px] 
+                bg-white rounded-md 
+                transition-all duration-300 origin-center 
+                lg:hidden
+                ${menuOpen ? 'hidden' : ''}`}>
+              </div>
+              <div className={`w-[65%] h-[3px] -translate-y-[0.65rem] bg-white rounded-md transition-all duration-300 origin-left lg:hidden
+                ${menuOpen ? 'rotate-[45deg]' : ''}`}>
+              </div>
           </label>
         </div>
 
@@ -37,16 +60,28 @@ const Navbar = () => {
         <ul className=
           {`${menuOpen ? 'top-[55px] opacity-100 grid bg-[#202124]' : 'hidden'} 
           z-[1] p-2 px-2 right-0 w-full absolute 
-          text-base 
+          text-base whitespace-nowrap
           rounded-b-md 
           gap-3
           transition-all 
-          lg:w-auto lg:flex lg:gap-3 lg:z-auto lg:static lg:p-0 lg:opacity-100 lg:top-0 lg:scale-100
-          xl:gap-10
+          lg:w-auto lg:flex lg:top-0 lg:p-0 
+          lg:z-auto lg:static 
+          lg:opacity-100 lg:scale-100
+          lg:gap-4 
+          xl:gap-10 
           `}>
-          <a href='#Products' className="flex flex-col items-center gap-1"><p>Products</p></a>
-          <a href='#AboutUs' className="flex flex-col items-center gap-1"><p>About Us</p></a>
-          <a href='#ContactUs' className="flex flex-col items-center gap-1"><p>Contact Us</p></a>
+          <a href='#Products' 
+            className="flex flex-col items-center gap-1">
+            Products
+          </a>
+          <a href='#AboutUs' 
+            className="flex flex-col items-center gap-1">
+            About Us
+          </a>
+          <a href='#ContactUs' 
+            className="flex flex-col items-center gap-1">
+            Contact Us
+          </a>
         </ul>
 
       </nav>
